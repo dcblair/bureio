3f11baf569fb683bbb81ee5a30a93cef21982f39
dblair962 <devin_blair@comcast.com>
2022-10-04 18:14:17 -0700

    Add route for dsii. Create redirect from index.

---
+++ app/routes/dsii/index.tsx
@@ -0,0 +1,10 @@
+export default function DreamSequenceTwo() {
+  return (
+    // bg color - 242 239 232 - #f2efe8
+    // For dark theme? 2f363b - 47 54 59
+    <div className="h-[100vh] bg-[#f2efe8]">
+      <svg />
+      <h1 className="m-10">bu.re_</h1>
+    </div>
+  );
+}
