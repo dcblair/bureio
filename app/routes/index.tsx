d1dd622f7f868a39ccfb785daed31b024518dc5a
dblair962 <devin_blair@comcast.com>
2022-10-04 17:20:09 -0700

    Scaffold out some ideas. Initialize Tailwind.

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,9 @@
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    // bg color - 242 239 232 - #f2efe8
+    // For dark theme? 2f363b - 47 54 59
+    <div className="">
+      <h1 className="ml-10">bu.re_</h1>
     </div>
   );
 }
