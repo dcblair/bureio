9950307bb78426f8666593279f82564dc9f7b2db
Devin B <devin.c.blair@gmail.com>
2024-10-30 01:43:16 -0700

    remove app.css. update tsconfig. fix entry.server.tsx.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [remix()],
+  build: {
+    minify: "esbuild",
+    cssMinify: "esbuild",
+  },
+  plugins: [!process.env.VITEST && remix(), tsconfigPaths()],
 });
