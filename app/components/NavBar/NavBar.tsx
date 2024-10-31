4dd6eed656bd64624321c2e526fb0d4a877ca4eb
Devin B <devin.c.blair@gmail.com>
2024-10-31 01:43:22 -0700

    decrease spacing. edit comments. format. update tooltip offset.

--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -2,22 +2,23 @@ import { Link, NavLink } from '@remix-run/react';
 import * as React from 'react';
 
 const BaseNavBar = () => {
-  const [isScroll, setIsScroll] = React.useState(false);
-
   return (
     <>
-      <div className="items-center justify-center py-4 text-center md:py-8">
-        {!isScroll && (
-          <div className="mt-4">
-            <Link
-              className="font-questrial text-4xl tracking-widest md:text-5xl"
-              to="/"
-            >
-              bu.re_
-            </Link>
-          </div>
-        )}
-        {/* TODO: Come back to this when  */}
+      <div className="items-center justify-center py-8 text-center md:pb-8 md:pt-12">
+        {/* header */}
+        <div>
+          <Link
+            className="font-questrial text-4xl tracking-widest md:text-5xl"
+            to="/"
+          >
+            bu.re_
+          </Link>
+        </div>
+
+        {/* divider */}
+        <div className="mt-4 h-0.5 w-1/2 rounded-r-sm bg-rich-black-fogra29" />
+
+        {/* TODO: Come back to this */}
         {/* <div className="fixed mt-4 w-full h-12 bg-white">
           <div className="flex items-center justify-evenly h-full py-4">
             <NavLink to="#">home</NavLink>
