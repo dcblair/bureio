1c5eb2200b6b758eed54bbb755642dbb3dc68d8f
Devin B <devin.c.blair@gmail.com>
2024-11-01 18:42:10 -0700

    improve notfound styling.

--- app/pages/NotFound.tsx
+++ app/pages/NotFound.tsx
@@ -1,23 +1,16 @@
-import { Link } from "@remix-run/react";
+import { Link } from '@remix-run/react';
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center text-center h-full w-full">
-      <div className="mb-10">
-        <iframe
-          allow="autoplay; fullscreen; picture-in-picture"
-          allowFullScreen
-          className="w-full h-full aspect-9/16"
-          frameBorder="0"
-          src="https://player.vimeo.com/video/759633300"
-          title="floating"
-        />
-      </div>
-      <div className="mb-8">
-        <div className="mb-4">
-          <span>something went wrong</span>
+    <div className="mt-0 flex size-full flex-col items-center justify-center text-center md:mt-40 md:justify-normal">
+      <div className="p-12 transition-all duration-2000 md:max-w-96 md:hover:shadow-5xl">
+        <div className="mb-8">
+          <span className="text-nowrap text-3xl">something went wrong</span>
         </div>
-        <Link className="underline font-bold" to="/">
+        <Link
+          className="text-base font-bold text-rich-black-fogra29/80 underline"
+          to="/"
+        >
           come back
         </Link>
       </div>
