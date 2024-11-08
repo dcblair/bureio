16e453398aa288a261b890c8b99e189bf9aaff75
Devin B <devin.c.blair@gmail.com>
2024-11-07 18:31:54 -0800

    add div underscore. add transition to underscore. apply resopnsive styling.

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -11,16 +11,19 @@ const BaseHeader = () => {
   return (
     <div className="relative mt-3 flex flex-col items-center justify-center pb-3 pt-1 text-center md:mt-12">
       {/* header */}
-      <Link to="/">
-        <h1 className="hover:animate-gradient-shift bg-gradient-to-r from-rich-black-fogra29 to-rich-black-fogra29/75 bg-clip-text font-questrial text-4xl tracking-widest text-transparent transition-all duration-1000 md:text-5xl">
-          bu.re_
+      <Link className="group relative" to="/">
+        <h1 className="bg-rich-black-fogra29 bg-clip-text font-questrial text-4xl tracking-widest text-transparent transition-all duration-3000 hover:bg-rich-black-fogra29/55 md:text-5xl">
+          bu.re<span className="inline md:hidden">_</span>
         </h1>
+
+        {/* underscore */}
+        <div className="rountded-sm absolute -right-10 bottom-1 hidden h-1 w-14 bg-gradient-to-l from-rich-black-fogra29 to-rich-black-fogra29/45 to-70% transition-all duration-2000 group-hover:w-32 md:flex" />
       </Link>
 
       {/* divider */}
       <div
         className={
-          'absolute bottom-1 left-0 mt-6 h-0.5 w-1/2 rounded-r-sm bg-gradient-to-r from-rich-black-fogra29 to-rich-black-fogra29/30 md:-bottom-3'
+          'absolute bottom-1 left-0 mt-6 h-0.5 w-1/2 rounded-r-sm bg-gradient-to-r from-rich-black-fogra29 to-rich-black-fogra29/30 md:-bottom-4'
         }
         ref={ref}
       />
