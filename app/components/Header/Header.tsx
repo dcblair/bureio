32510730a601bc6b51ddb3223ec6233bef2b40c3
Devin B <devin.c.blair@gmail.com>
2024-11-08 17:31:09 -0800

    re-revert to custom link.

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import { NavLink } from '@remix-run/react';
-import { Link } from '@remix-run/react';
+import { Link } from '~/components';
 import * as React from 'react';
 import { useIntersectionObserver } from '~/hooks';
 
@@ -12,12 +12,12 @@ const BaseHeader = () => {
     <div className="relative mt-3 flex flex-col items-center justify-center pb-3 pt-1 text-center md:mt-12">
       {/* header */}
       <Link className="group relative" to="/">
-        <h1 className="bg-clip-text-safari bg-rich-black-fogra29 font-questrial text-4xl tracking-widest text-transparent transition-all duration-3000 hover:bg-rich-black-fogra29/55 md:text-5xl">
+        <h1 className="bg-rich-black-fogra29 bg-clip-text font-questrial text-4xl tracking-widest text-transparent transition-all duration-3000 hover:bg-rich-black-fogra29/55 md:text-5xl">
           bu.re<span className="inline md:hidden">_</span>
         </h1>
 
         {/* underscore */}
-        <div className="rountded-sm absolute -right-14 bottom-1 hidden h-1 w-14 bg-gradient-to-l from-rich-black-fogra29 to-rich-black-fogra29/45 to-70% transition-all duration-2000 group-hover:w-32 md:flex" />
+        <div className="rountded-sm absolute -right-10 bottom-1 hidden h-1 w-14 bg-gradient-to-l from-rich-black-fogra29 to-rich-black-fogra29/45 to-70% transition-all duration-2000 group-hover:w-32 md:flex" />
       </Link>
 
       {/* divider */}
